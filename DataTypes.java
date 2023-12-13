d5e6182b739815edc3155e6df5d0b86ebc9b0bfe
sangeeta9538 <sangeetak.xworkz@gmail.com>
2023-12-13 14:23:45 +0530

    Simple Programs

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class DataTypes
+{
+    public static void main(String[] args) {
+        int numOfPens=10;
+        System.out.println(numOfPens);
+        numOfPens=100;
+        System.out.println(numOfPens);
+        
+        numOfPens=20;
+        System.out.println(numOfPens);
+
+
+
+    }
+}
